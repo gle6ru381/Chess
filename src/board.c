efe67ee01b499569fe57bf1485015452021768ca
Gleb <trogaev@bk.ru>
2020-03-08 18:44:22 +0700

    add leak protection

--- src/board.c
+++ src/board.c
@@ -9,7 +9,13 @@ void map_free(Map map)
 String* string_new(size_t size)
 {
     String* string = (String*)(malloc(sizeof(String)));
+    if (string == NULL)
+        return NULL;
     string->str = (char*)(calloc(size, sizeof(char)));
+    if (string->str == NULL) {
+        free(string);
+        return NULL;
+    }
     string->capacity = size;
     string->size = 0;
     return string;
@@ -37,6 +43,8 @@ int string_push_back(String* str, char c)
 {
     if (str->size == str->capacity) {
         str->str = realloc(str->str, (str->capacity * 2) * sizeof(char));
+        if (str->str == NULL)
+            return -1;
         str->capacity *= 2;
     }
 
--- src/board_read.c
+++ src/board_read.c
@@ -4,23 +4,35 @@ Map board_read()
 {
     Map map;
     String* first = string_new(5);
+    if (!first)
+        exit(EXIT_FAILURE);
     String* second = string_new(5);
+    if (!second)
+        exit(EXIT_FAILURE);
     String* buff = string_new(10);
+    if (!buff)
+        exit(EXIT_FAILURE);
     char c;
     while ((c = getchar()) != '\n') {
-        string_push_back(buff, c);
+        if ((string_push_back(buff, c)) == -1)
+            exit(EXIT_FAILURE);
     }
-    string_push_back(buff, '\0');
+    if ((string_push_back(buff, '\0')) == -1)
+        exit(EXIT_FAILURE);
     int i = 0;
     for (; buff->str[i] != ' ' && buff->str[i] != '-'; i++) {
-        string_push_back(first, buff->str[i]);
+        if ((string_push_back(first, buff->str[i])) == -1)
+            exit(EXIT_FAILURE);
     }
-    string_push_back(first, '\0');
+    if ((string_push_back(first, '\0')) == -1)
+        exit(EXIT_FAILURE);
     i++;
     for (; buff->str[i] != '\0'; i++) {
-        string_push_back(second, buff->str[i]);
+        if ((string_push_back(second, buff->str[i])) == -1)
+            exit(EXIT_FAILURE);
     }
-    string_push_back(second, '\0');
+    if ((string_push_back(second, '\0')) == -1)
+        exit(EXIT_FAILURE);
 
     map.first = first;
     map.second = second;
